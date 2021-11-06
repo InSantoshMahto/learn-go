3b97d494ce80bf4a0f186b2e6b4f5f49439be588
SANTOSH MAHTO <mahtos480@gmail.com>
2021-11-06 16:54:35 +0530

    learn ifelse, switchcase, loop and function

---
+++ 15functions/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Welcome to function in golang")
+	greeter()
+
+	result := adder(3, 5)
+	fmt.Println("Result is: ", result)
+
+	proResult, _ := proAdder(1, 2, 3, 4, 5, 2, 4, 6, 7, 8)
+	fmt.Println("Pro Result is: ", proResult)
+}
+
+func greeter() {
+	fmt.Println("Namstey from golang")
+}
+
+func adder(valOne int, valTwo int) int {
+	return valOne + valTwo
+}
+
+func proAdder(values ...int) (int, string) {
+	total := 0
+	for _, val := range values {
+		total += val
+	}
+	return total, "Got Result"
+}
